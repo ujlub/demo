7de2d557e1386e295a7343ce9c9634cf01be0e0c
154510 <154510@ssg.com>
2025-03-07 13:16:48 +0900

    init springboot demo

---
+++ src/main/java/com/example/demo/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        return "main";
+    }
+}
